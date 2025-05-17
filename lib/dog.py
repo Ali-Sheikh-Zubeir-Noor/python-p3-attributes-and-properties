5d14332e6487d343819503bda5e69c6bcec81b01
Ali Sheikh Zubeir Noor <ali.noor1@student.moringaschool.com>
2025-05-17 22:26:35 +0300

    Adds changes

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +10,38 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    approved_breeds = APPROVED_BREEDS
+
+    def __init__(self, name="Any", breed="Beagle"): 
+        self.name = name        
+        self.breed = breed      
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and (1 <= len(value) <= 25):
+            self._name = value
+        else:
+            raise ValueError("Name must be string between 1 and 25 characters")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in Dog.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None  
+
+
+
+
+        
+    
+        
+        
